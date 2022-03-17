f0d4675065b87a69c86870be8925d888b7921128
cmenasse <cmenasse@student.42.fr>
2022-03-17 14:49:17 +0100

    Completed lab

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const mongoose = require('mongoose');
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require('./data.json');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+
+const testRecipe = {
+  title: "first",
+  level: "Easy Peasy",
+  ingredients: ["milk"],
+  cuisine: "French",
+  dishType: "drink",
+  duration: 1,
+  creator: "me"
+}
 
 // Connection to the database "recipe-app"
 mongoose
@@ -15,9 +25,17 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+  .then(async () => {
     // Run your code here, after you have insured that the connection was made
+    const recipes = await Recipe.insertMany(data)
+    recipes.forEach(x => console.log(x.title))
+    await Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"},  {duration: 100}, {new: true})
+    console.log("update success:")
+    await Recipe.deleteOne({title: "Carrot Cake"})
+    console.log("delete success!")
+    mongoose.disconnect()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
